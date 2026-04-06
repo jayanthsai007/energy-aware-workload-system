4966ff5457e74bd12a8a48565d72b74f7818a9a1
Ankitha Yelley <ankithayelley@gmail.com>
2026-04-06 20:33:14 +0530

    fix: root.tsx issue

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,9 +1,7 @@
-import { type RouteConfig, layout, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  layout("root.tsx", [
-    index("routes/index.tsx"),
-    route("metrics", "routes/metrics.tsx"),
-    route("detail/:nodeId", "routes/detail.$nodeId.tsx"),
-  ]),
+  index("routes/index.tsx"),
+  route("metrics", "routes/metrics.tsx"),
+  route("detail/:nodeId", "routes/detail.$nodeId.tsx"),
 ] satisfies RouteConfig;
