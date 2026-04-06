e25bb791b9c7b286a0de7ec53f3522327b2213e2
Ankitha Yelley <ankithayelley@gmail.com>
2026-04-06 20:43:35 +0530

    fix: cors error

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,9 +16,20 @@ from app.api.routes_execution_metrics import router as execution_metrics_router
 import threading
 import time
 from datetime import datetime, timedelta
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
